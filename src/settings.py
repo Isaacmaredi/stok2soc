7c4c6b89b2062433f3c68b6f4531e8b1102f35a6
isaacmaredi <isaacmaredi@icloud.com>
2022-06-29 22:14:25 +0200

    locals settings

--- src/settings.py
+++ src/settings.py
@@ -151,3 +151,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+try:
+    from .local_settings import *
+except:
+    pass
